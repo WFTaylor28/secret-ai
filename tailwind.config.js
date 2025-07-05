4290bd82bfce53ae684a867984eba8674bd7c589
WFTaylor28 <wftaylor6550@gmail.com>
2025-07-05 17:53:26 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#007BFF',
+        secondary: '#6C63FF',
+        darkBg: '#121212',
+        lightBg: '#F9FAFB',
+      },
+    },
+  },
+  plugins: [],
+}
